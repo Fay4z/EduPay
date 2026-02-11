8459b18601d6ee4668a5544ee73cb2c1d5132d83
Fay4z <fayazjr212@gmail.com>
2026-02-11 10:23:10 +0530

    inital client setup with shadcn, tailwind css, react done

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>Setup success</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
